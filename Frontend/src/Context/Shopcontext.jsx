577229dc20541828d34562092d0ddec2fc04a2f8
Ayush Verma <ayve012@gmail.com>
2025-03-25 18:26:56 +0530

    Update Shopcontext.jsx

--- Frontend/src/Context/Shopcontext.jsx
+++ Frontend/src/Context/Shopcontext.jsx
@@ -15,12 +15,12 @@ const ShopcontextProvider = (props) => {
   const [cartItems, setCartItems] = useState(getDefaultCart());
 
   useEffect(() => {
-    fetch("http://localhost:4000/allproducts")
+    fetch("https://e-commerce-shopping-site-using-mern.onrender.com/allproducts")
       .then((response) => response.json())
       .then((data) => setAll_Product(data));
 
     if (localStorage.getItem("auth-token")) {
-      fetch("http://localhost:4000/getcart", {
+      fetch("https://e-commerce-shopping-site-using-mern.onrender.com/getcart", {
         method: "POST",
         headers: {
           Accept: "application/form-data",
@@ -40,7 +40,7 @@ const ShopcontextProvider = (props) => {
       [itemId]: prev[itemId] + 1,
     }));
     if (localStorage.getItem("auth-token")) {
-      fetch("http://localhost:4000/addtocart", {
+      fetch("https://e-commerce-shopping-site-using-mern.onrender.com/addtocart", {
         method: "POST",
         headers: {
           Accept: "application/form-data",
@@ -60,7 +60,7 @@ const ShopcontextProvider = (props) => {
       [itemId]: prev[itemId] - 1,
     }));
     if (localStorage.getItem("auth-token")) {
-      fetch("http://localhost:4000/removefromcart", {
+      fetch("https://e-commerce-shopping-site-using-mern.onrender.com/removefromcart", {
         method: "POST",
         headers: {
           Accept: "application/form-data",
