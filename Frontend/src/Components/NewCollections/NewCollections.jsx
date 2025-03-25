afd49a4ad979f306007b4e2c118a903a0d25dc8e
Ayush Verma <ayve012@gmail.com>
2025-03-25 18:30:58 +0530

    Update NewCollections.jsx

--- Frontend/src/Components/NewCollections/NewCollections.jsx
+++ Frontend/src/Components/NewCollections/NewCollections.jsx
@@ -7,7 +7,7 @@ const NewCollections = () => {
   const [new_collection, setNew_collection] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:4000/newcollectioned")
+    fetch("https://e-commerce-shopping-site-using-mern.onrender.com/newcollectioned")
       .then((response) => response.json())
       .then((data) => setNew_collection(data));
   }, []);
